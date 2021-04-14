fad1d8b2e29500c48a399af6d926c86da528b547
AnSenSei <74482194+AnSenSei@users.noreply.github.com>
2021-04-14 15:06:01 -0400

    Update Linear_Classifiers1.py

--- Linear_Classifiers1.py
+++ Linear_Classifiers1.py
@@ -3,6 +3,7 @@ import numpy as np
 import math
 import argparse
 import scipy.io as sio
+import pandas as pd
 
 def data_loader(data_name):
     if data_name == "starplus":
@@ -22,16 +23,16 @@ def data_loader(data_name):
         #meta.rois is a struct array defining a few dozen anatomically defined Regions Of Interest (ROIs) in the brain. Each element of the struct array defines on of the ROIs, and has three fields: "name" which gives the ROI name (e.g., 'LIFG'), "coords" which gives the xyz coordinates of each voxel in that ROI, and "columns" which gives the column index of each voxel in that ROI.
         #meta.colToROI{v} gives the ROI of the voxel corresponding to column v in the data.
         """
-        # study      = metadata['study']
-        # subject    = metadata['subject']
-        # ntrials    = metadata['ntrials'][0][0]
-        # nsnapshots = metadata['nsnapshots'][0][0]
-        # dimx       = metadata['dimx'][0][0]
-        # colToCoord = metadata['colToCoord']
-        # coordToCol = metadata['coordToCol']
-        # rois       = metadata['rois']
-        # colToROI   = metadata['colToROI']
-        # info = starplus['info'][0]
+        study      = metadata['study']
+        subject    = metadata['subject']
+        ntrials    = metadata['ntrials'][0][0]
+        nsnapshots = metadata['nsnapshots'][0][0]
+        dimx       = metadata['dimx'][0][0]
+        colToCoord = metadata['colToCoord']
+        coordToCol = metadata['coordToCol']
+        rois       = metadata['rois']
+        colToROI   = metadata['colToROI']
+        info = starplus['info'][0]
         #info: This variable defines the experiment in terms of a sequence of 'trials'. 'info' is a 1x54 struct array, describing the 54 time intervals, or trials. Most of these time intervals correspond to trials during which the subject views a single picture and a single sentence, and presses a button to indicate whether the sentence correctly describes the picture. Other time intervals correspond to rest periods. The relevant fields of info are illustrated in the following example:
         #info(18) mint: 894 maxt: 948 cond: 2 firstStimulus: 'P' sentence: ''It is true that the star is below the plus.'' sentenceRel: 'below' sentenceSym1: 'star' sentenceSym2: 'plus' img: sap actionAnswer: 0 actionRT: 3613
         #info.mint gives the time of the first image in the interval (the minimum time)
@@ -42,34 +43,34 @@ def data_loader(data_name):
         #info.img describes the image presented during this trial. For example, 'sap' means the image contained a 'star above plus'. Each image has two tokens, where one is above the other. The possible tokens are star (s), plus (p), and dollar (d).
         #info.actionAnswer: has values -1 or 0. A value of 0 indicates the subject is expected to press the answer button during this trial (either the 'yes' or 'no' button to indicate whether the sentence correctly describes the picture). A value of -1 indicates it is inappropriate for the subject to press the answer button during this trial (i.e., it is a rest, or fixation trial).
         #info.actionRT: gives the reaction time of the subject, measured as the time at which they pressed the answer button, minus the time at which the second stimulus was presented. Time is in milliseconds. If the subject did not press the button at all, the value is 0.
-        # data = starplus['data']
+        data = starplus['data']
         #data: This variable contains the raw observed data. The fMRI data is a sequence of images collected over time, one image each 500 msec. The data structure 'data' is a [54x1] cell array, with one cell per 'trial' in the experiment. Each element in this cell array is an NxV array of observed fMRI activations. The element data{x}(t,v) gives the fMRI observation at voxel v, at time t within trial x. Here t is the within-trial time, ranging from 1 to info(x).len. The full image at time t within trial x is given by data{x}(t,:).
         #Note the absolute time for the first image within trial x is given by info(x).mint.
-        # MIN_LOSS_CHANGE=0.0001
-        # maxFeatures = max([data[i][0].flatten().shape[0] for i in range(data.shape[0])])
+        MIN_LOSS_CHANGE=0.0001
+        maxFeatures = max([data[i][0].flatten().shape[0] for i in range(data.shape[0])])
 
-        # Inputs
-        # X = np.zeros((ntrials, maxFeatures + 1))
-        # for i in range(data.shape[0]):
-        #     f = data[i][0].flatten()
-        #     X[i, :f.shape[0]] = f
-        #     X[i, f.shape[0]] = 1  # Bias
 
-        # # Outputs (+1 = Picture, -1 = Sentence)
-        # Y = np.ones(ntrials)
-        # Y[np.array([info[i]['firstStimulus'][0] != 'P' for i in range(ntrials)])] = -1
+        X = np.zeros((ntrials, maxFeatures + 1))
+        for i in range(data.shape[0]):
+            f = data[i][0].flatten()
+            X[i, :f.shape[0]] = f
+            X[i, f.shape[0]] = 1  # Bias
+
+         # Outputs (+1 = Picture, -1 = Sentence)
+        Y = np.ones(ntrials)
+        Y[np.array([info[i]['firstStimulus'][0] != 'P' for i in range(ntrials)])] = -1
 
         # # Randomly permute the data
-        # np.random.seed(1)  # Seed the random number generator to preserve the dev/test split
-        # permutation = np.random.permutation(ntrials)
-        # permutation = np.random.permutation(X.shape[0])
-        # X = X[permutation,]
-        # Y = Y[permutation,]
-        # X_train = X[:40]
-        # X_test = X[40:]
-        # Y_train = Y[:40]
-        # Y_test = Y[40:]
-        # np.savez('Starplus.npz', X_train=X_train, X_test=X_test, Y_train=Y_train,Y_test=Y_test)
+        np.random.seed(1)  # Seed the random number generator to preserve the dev/test split
+        permutation = np.random.permutation(ntrials)
+        permutation = np.random.permutation(X.shape[0])
+        X = X[permutation,]
+        Y = Y[permutation,]
+        X_train = X[:40]
+        X_test = X[40:]
+        Y_train = Y[:40]
+        Y_test = Y[40:]
+        np.savez('Starplus.npz', X_train=X_train, X_test=X_test, Y_train=Y_train,Y_test=Y_test)
         
         data = np.load('Starplus.npz')
         X_train = data['X_train'] / 10.0
@@ -160,22 +161,22 @@ def Accuracy(X, Y, w):
 
 def main(args):
     X_train, Y_train, X_test, Y_test = data_loader(args.data)
-    # print("number of training data instances: ", X_train.shape)
-    # print("number of test data instances: ", X_test.shape)
-    # print("number of training data labels: ", Y_train.shape)
-    # print("number of test data labels: ", Y_test.shape)
+    print("number of training data instances: ", X_train.shape)
+    print("number of test data instances: ", X_test.shape)
+    print("number of training data labels: ", Y_train.shape)
+    print("number of test data labels: ", Y_test.shape)
 
-    # if args.algorithm == "logistic":
-    # #----------------Logistic Loss-----------------------------------
-    #     w = Logisitc_Regression(X_train, Y_train,  maxIter=100, learningRate=0.1)
-    # # ----------------Perceptron-----------------------------------
-    # else:
-    #     w = Perceptron(X_train, Y_train, maxIter=100, learningRate=0.1)
+    if args.algorithm == "logistic":
+     #----------------Logistic Loss-----------------------------------
+        w = Logisitc_Regression(X_train, Y_train,  maxIter=100, learningRate=0.1)
+     # ----------------Perceptron-----------------------------------
+    else:
+        w = Perceptron(X_train, Y_train, maxIter=100, learningRate=0.1)
 
-    # training_accuracy = Accuracy(X_train, Y_train, w)
-    # test_accuracy = Accuracy(X_test, Y_test, w)
-    # print("Accuracy: training set: ", training_accuracy)
-    # print("Accuracy: test set: ", test_accuracy)
+    training_accuracy = Accuracy(X_train, Y_train, w)
+    test_accuracy = Accuracy(X_test, Y_test, w)
+    print("Accuracy: training set: ",training_accuracy)
+    print("Accuracy: test set: ",test_accuracy)
     
 
 if __name__ == "__main__":
